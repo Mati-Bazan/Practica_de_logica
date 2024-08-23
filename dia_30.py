45b994da63e2c21f9e8618ef64e61d52dfc31a24
Matias Bazan <mati.bazan97@gmail.com>
2024-08-23 19:17:29 -0300

    Dia 30

--- dia_30.py
+++ dia_30.py
@@ -54,3 +54,45 @@ lamp = Lamp(LampSwitch())
 lamp.operate("on")
 lamp.operate("off")
 
+"""
+ Crea un sistema de notificaciones.
+ Requisitos:
+* 1. El sistema puede enviar Email, PUSH y SMS (implementaciones específicas).
+* 2. El sistema de notificaciones no puede depender de las implementaciones específicas.
+Instrucciones:
+* 1. Crea la interfaz o clase abstracta.
+* 2. Desarrolla las implementaciones específicas.
+* 3. Crea el sistema de notificaciones usando el DIP.
+* 4. Desarrolla un código que compruebe que se cumple el principio.
+"""
+from abc import ABC, abstractmethod
+
+class Notifier(ABC):
+    def send (self, message: str):
+        pass
+
+class EmailNotifier(Notifier):
+    def send(self, message: str):
+        print(f"Enviando email, {message}")
+
+class PUSHNotifier(Notifier):
+    def send(self, message: str):
+        print(f"Enviando PUSH, {message}")
+
+class SMSNotifier(Notifier):
+    def send(self, message: str):
+        print(f"Enviando SMS, {message}") 
+
+
+class NotificationService:
+
+    def __init__(self, notifier: Notifier) -> None:
+        self.notifier = notifier
+
+    def notify(self, message: str):
+        self.notifier.send(message)
+
+# service = NotificationService(EmailNotifier())
+# service = NotificationService(PUSHNotifier())
+service = NotificationService(SMSNotifier())
+service.notify("Hola")
