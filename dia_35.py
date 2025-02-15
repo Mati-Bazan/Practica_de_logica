43b6b2bd0108c2b690bc4c8a21bbd7362ea1ba8b
Matias Bazan <mati.bazan97@gmail.com>
2025-02-15 20:29:46 -0300

    Implement ring distribution logic in dia_35.py with input handling

--- dia_35.py
+++ dia_35.py
@@ -14,3 +14,32 @@
 *    y busque una posible combinación para repartirlos.
 * 2. Muestra el reparto final o el error al realizarlo.
 """
+
+def distribute_rings(total_rings: int):
+    
+    sauron = 1
+    total_rings -= sauron # sauron
+
+    for men in range(2,total_rings,2):
+        for elves in range(1,total_rings,2):
+            dwarves = total_rings - men - elves
+
+            return {
+                "Hombres":men,
+                "Elfos":elves,
+                "Enanos":dwarves,
+                "Sauron":1
+            }
+
+    
+
+    return "No es posible repartir los anillos."
+
+
+
+try:
+    total_rings = int(input("Ingresa el número total de anillos: "))
+    distributed_rings = distribute_rings(total_rings)
+    
+except ValueError:
+    print("Ingresa un número válido de anillos.")
